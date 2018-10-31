a6ea0c8cd9dd6fe31deec631eefcecd8d97078e8
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2018-10-31 10:14:06 +0000

    debugged exercise4.rb

---
+++ exercise4.rb
@@ -0,0 +1,28 @@
+def encode(plaintext, key)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+  ciphertext_chars = plaintext.chars.map do |char|
+    (65 + cipher.find_index(char)).chr
+  end
+  ciphertext_chars.join
+end
+
+def decode(ciphertext, key)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+  p cipher
+
+  plaintext_chars = ciphertext.chars.map do |char|
+    p "The cipher letter is #{char}, and it is decoding to character number #{(65 - char.ord).abs}, which is #{cipher [(65 - char.ord).abs]}"
+    
+    # cipher[65 - char.ord]
+    cipher [(65 - char.ord).abs]
+  end
+  plaintext_chars.join
+end
+
+# Intended output:
+#
+# > encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+# => "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
+#
+# > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+# => "theswiftfoxjumpedoverthelazydog"
